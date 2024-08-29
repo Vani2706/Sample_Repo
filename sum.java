088f018ec8c365ad1878952947860677ae3f349e
Rai Vani Harapriya <raivaniharapriya1902@gmail.com>
2024-08-29 11:06:26 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,8 +1,10 @@
-package com.verizon
+
   public class Sum{
     public Integer addNum(){
+      public static void main(String[] args){
       Integer a=9;
       Integer b=8;
       return a+b;
+      }
     }
   }
