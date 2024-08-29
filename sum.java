685c93af0aed65833123534efd462a9497071840
Rai Vani Harapriya <raivaniharapriya1902@gmail.com>
2024-08-29 11:07:49 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,10 +1,9 @@
 
   public class Sum{
-    public Integer addNum(){
       public static void main(String[] args){
       Integer a=9;
       Integer b=8;
       return a+b;
       }
     }
-  }
+  
